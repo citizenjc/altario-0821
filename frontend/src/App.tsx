e3136823cd02a520b0d1adb37708191d1fc36905
Joao Campos <joaoc@evoke.pt>
2023-09-22 18:43:37 +0100

    more fixes to base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home/Home";
+import { Generator } from "./components/Generator/Generator";
+import { Payment } from "./components/Payment/Payment";
+import { Layout, NoMatch } from "./components/Layout/Layout";
 
-export default function App() {
+export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -12,63 +16,4 @@ export default function App() {
       </Route>
     </Routes>
   );
-}
-
-function Layout() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/generator">Generator</Link>
-          </li>
-          <li>
-            <Link to="/payment">Payment</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      <Outlet />
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function Generator() {
-  return (
-    <div>
-      <h2>Generator</h2>
-    </div>
-  );
-}
-
-function Payment() {
-  return (
-    <div>
-      <h2>Payment</h2>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
-}
+};
